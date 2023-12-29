3baadfd950928a2010d2e1133c3d4c1ab2b3d59a
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2023-12-29 14:00:00 -0600

    added file folder structure

--- AExampleLibrary/Package.swift
+++ AExampleLibrary/Package.swift
@@ -5,17 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "AExampleLibrary",
+//    platforms: [
+//      .iOS(.v16)
+//    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AExampleLibrary",
             targets: ["AExampleLibrary"]),
     ],
+    dependencies: [
+//      .package(url: "https://github.com/pointfreeco/swift-composable-architecture", branch: "observable-beta"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AExampleLibrary"),
+            name: "AExampleLibrary",
+            dependencies: [
+//                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
         .testTarget(
             name: "AExampleLibraryTests",
             dependencies: ["AExampleLibrary"]),
