03e6e99347b8099146e4c851e7387149ee35b297
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2023-12-29 13:12:45 -0600

    Initial Commit

---
+++ XCFrameworkForArchive/XCFrameworkForArchive.h
@@ -0,0 +1,18 @@
+//
+//  XCFrameworkForArchive.h
+//  XCFrameworkForArchive
+//
+//  Created by Joseph Goodrick on 12/29/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCFrameworkForArchive.
+FOUNDATION_EXPORT double XCFrameworkForArchiveVersionNumber;
+
+//! Project version string for XCFrameworkForArchive.
+FOUNDATION_EXPORT const unsigned char XCFrameworkForArchiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCFrameworkForArchive/PublicHeader.h>
+
+
